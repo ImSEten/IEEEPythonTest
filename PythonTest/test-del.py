cb79a92c298f65b60830dacafa4f391f17c821d7
Immortal Saber <Immortal_Saber@163.com>
2019-05-15 12:42:42 +0800

    update on Desktop

--- PythonTest/test-del.py
+++ PythonTest/test-del.py
@@ -39,42 +39,104 @@ def FootballDynasty():
         for y in range(4, num - 1):#移位
             players[y] = players[y + 1]
         players[num - 1] = tempchange
-        if count >= max:#output添加一项
+        ###bug
+        if count > max:#直接覆盖掉output
+            max = count#更新max
+            if x + 1 - count == 0:
+                if games[x] == 'W':#白队连胜
+                    if (count + 2) % 2 == 0:#胜利偶数场，直接保存当前顺序
+                        output = [players[0] + ' ' + players[2]]
+                    else:#胜利奇数场
+                        output = [players[2] + ' ' + players[0]]
+                else:#黑队连胜
+                    if (count + 2) % 2 == 0:#胜利偶数场，直接保存当前顺序
+                        output = [players[1] + ' ' + players[3]]
+                    else:#胜利奇数场
+                        output = [players[3] + ' ' + players[1]]
+            else:
+                if games[x] == 'W':#白队连胜
+                    if (count + 2) % 2 > 0:#胜利奇数场，直接保存当前顺序
+                        output = [players[0] + ' ' + players[2]]
+                    else:#胜利偶数场
+                        output = [players[2] + ' ' + players[0]]
+                else:#黑队连胜
+                    if (count + 2) % 2 > 0:#胜利奇数场，直接保存当前顺序
+                        output = [players[1] + ' ' + players[3]]
+                    else:#胜利偶数场
+                        output = [players[3] + ' ' + players[1]]
+        elif count == max:#output添加一项
             max = count
-            if games[x] == 'W':#白队连胜
-                if (count + 2) % 2 == 0:#胜利偶数场，直接保存当前顺序
-                    setoutput = players[0] + ' ' + players[2]#当前顺序
-                    for m in range(len(output)):
-                        if output[m] == setoutput:
-                            same = True#有相同的
-                            break
-                    if same != True:
-                        output.append(setoutput)
-                else:#胜利奇数场
-                    setoutput = players[2] + ' ' + players[0]#交换顺序
-                    for m in range(len(output)):
-                        if output[m] == setoutput:#有相同的
-                            same = True
-                            break
-                    if same != True:
-                        output.append(setoutput)
-            else:#黑队连胜
-                if (count + 2) % 2 == 0:#胜利偶数场，直接保存当前顺序
-                    setoutput = players[1] + ' ' + players[3]
-                    for m in range(len(output)):
-                        if output[m] == setoutput:
-                            same = True#有相同的
-                            break
-                    if same != True:
-                        output.append(setoutput)
-                else:#胜利奇数场
-                    setoutput = players[3] + ' ' + players[1]
-                    for m in range(len(output)):
-                        if output[m] == setoutput:
-                            same = True#有相同的
-                            break
-                    if same != True:
-                        output.append(setoutput)
+            if x + 1 - count == 0:#从第一局开始一直连胜
+                #偶数场保存当前顺序，奇数场交换顺序
+                if games[x] == 'W':#白队连胜
+                    if (count + 2) % 2 == 0:#胜利偶数场，直接保存当前顺序
+                        setoutput = players[0] + ' ' + players[2]#当前顺序
+                        for m in range(len(output)):
+                            if output[m] == setoutput:
+                                same = True#有相同的
+                                break
+                        if same != True:
+                            output.append(setoutput)
+                    else:#胜利奇数场，交换顺序
+                        setoutput = players[2] + ' ' + players[0]#交换顺序
+                        for m in range(len(output)):
+                            if output[m] == setoutput:#有相同的
+                                same = True
+                                break
+                        if same != True:
+                            output.append(setoutput)
+                else:#黑队连胜
+                    if (count + 2) % 2 == 0:#胜利偶数场，直接保存当前顺序
+                        setoutput = players[1] + ' ' + players[3]
+                        for m in range(len(output)):
+                            if output[m] == setoutput:
+                                same = True#有相同的
+                                break
+                        if same != True:
+                            output.append(setoutput)
+                    else:#胜利奇数场，交换顺序
+                        setoutput = players[3] + ' ' + players[1]
+                        for m in range(len(output)):
+                            if output[m] == setoutput:
+                                same = True#有相同的
+                                break
+                        if same != True:
+                            output.append(setoutput)
+            else:
+                if games[x] == 'W':#白队连胜
+                    if (count + 2) % 2 > 0:#胜利奇数场，直接保存当前顺序
+                        setoutput = players[0] + ' ' + players[2]#当前顺序
+                        for m in range(len(output)):
+                            if output[m] == setoutput:
+                                same = True#有相同的
+                                break
+                        if same != True:
+                            output.append(setoutput)
+                    else:#胜利偶数场，交换顺序
+                        setoutput = players[2] + ' ' + players[0]#交换顺序
+                        for m in range(len(output)):
+                            if output[m] == setoutput:#有相同的
+                                same = True
+                                break
+                        if same != True:
+                            output.append(setoutput)
+                else:#黑队连胜
+                    if (count + 2) % 2 > 0:#胜利奇数场，直接保存当前顺序
+                        setoutput = players[1] + ' ' + players[3]
+                        for m in range(len(output)):
+                            if output[m] == setoutput:
+                                same = True#有相同的
+                                break
+                        if same != True:
+                            output.append(setoutput)
+                    else:#胜利偶数场，交换顺序
+                        setoutput = players[3] + ' ' + players[1]
+                        for m in range(len(output)):
+                            if output[m] == setoutput:
+                                same = True#有相同的
+                                break
+                        if same != True:
+                            output.append(setoutput)
         front = games[x]#更新前一个胜利者
     for x in range(len(output)):
         print(output[x])
